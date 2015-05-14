0ea7da02c5b0cf723210fb8e8af9f21e11d2e703
Hayden Guthrie <seehaydeng@gmail.com>
2015-05-14 11:06:06 -0700

    Milestone 10

--- index.php
+++ index.php
@@ -17,12 +17,24 @@ define('ImageDirectory', 'pics/');
 function connectToInstagram($url) {
 	$ch = curl_init();
 
-	curl_setopt_array($ch, array[
+	curl_setopt_array($ch, array(
 		CURLOPT_URL => $url,
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_SSL_VERIFYPEER => false,
-		CURLOPT_SSL_VERIFYHOST => 2
-	]);
+		CURLOPT_SSL_VERIFYHOST => 2,
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
+
+//function to get userid cause username doesnt allow us to get pictures
+function getUserID($userName) {
+	$url = 'http://api.instagram.com/v1/users/search?q=' . $userName . '&client_id=' . clientID;
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+
+	echo $results['data']['0']['id'];
 }
 
 if (isset($_GET['code'])) {
@@ -37,18 +49,20 @@ if (isset($_GET['code'])) {
 	//cURL is what we use in php, library for calls to other apis
 	$curl = curl_init($url); //setting curl session, get data from url
 	curl_setopt($curl, CURLOPT_POST, true);	
-	curl_setopt($curl, CURLOPT_POSTFIELDS, access_token_settings); //setting POSTFIELDS to array setup we created
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); //setting POSTFIELDS to array setup we created
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting equal to 1, getting strings back
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //work, set to true
 
 
-$result = curl_exec($curl);
-curl_close();
+	$result = curl_exec($curl);
+	curl_close($curl);
 
+	$results = json_decode($result, true);
+	getUserID($results['user']['username']);
 }
 
 else {
-
+}
 ?>
 
 <!DOCTYPE html>
@@ -65,6 +79,3 @@ else {
 	</body>
 </html>
 
-<?php 
-
-}
